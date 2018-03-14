ee4a4f83725b20f99911ea9bb20b83ba862fb9e2
tanzanlong <15218835680@163.com>
2018-03-14 19:21:48 +0800

    mq demo

--- activemq-simple/src/main/java/com/baibei/accountservice/Bootstrap.java
+++ activemq-simple/src/main/java/com/baibei/accountservice/Bootstrap.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-import com.baibei.accountservice.filter.DynamicDataSourceFilter;
-import com.restMvcFilter.MvcTraceFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.FilterRegistrationBean;
@@ -18,6 +16,7 @@ import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.baibei.log4k8s.Log4k8s;
+import com.restMvcFilter.MvcTraceFilter;
 
 /**
  * Created by l_yy on 2017/3/9. clean deploy -Dmaven.test.skip=true
@@ -50,8 +49,6 @@ public class Bootstrap extends WebMvcConfigurerAdapter implements SchedulingConf
     @Bean  
     public FilterRegistrationBean  filterRegistrationBean() {  
         FilterRegistrationBean registrationBean = new FilterRegistrationBean();  
-        DynamicDataSourceFilter dynamicDataSourceFilter = new DynamicDataSourceFilter();
-        registrationBean.setFilter(dynamicDataSourceFilter);  
         List<String> urlPatterns = new ArrayList<String>();  
         urlPatterns.add("/*");  
         registrationBean.setUrlPatterns(urlPatterns);  
--- rocketmq-simple/src/main/java/com/baibei/accountservice/paycenter/service/WithdrawService.java
+++
@@ -1,5 +0,0 @@
-package com.baibei.accountservice.paycenter.service;
-
-public class WithdrawService {
-
-}
